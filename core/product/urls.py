2d160bb50d92854b7fadb8cd84282ad55bba376c
jaimindv <jaimindave24@gmail.com>
2025-03-03 01:39:47 +0530

    integrated product bulk upload API

--- core/product/urls.py
+++ core/product/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProductViewSet
+from .views import BulkUploadAPIView, ProductViewSet
 
 router = DefaultRouter()
 router.register(r"", ProductViewSet, basename="products")
 
 urlpatterns = [
+    path("upload/", BulkUploadAPIView.as_view(), name="upload"),
     path("", include(router.urls)),
 ]
--- core/product/views.py
+++ core/product/views.py
@@ -1,8 +1,12 @@
+import json
+
 from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from base.permissions import IsAdmin, IsAPIKeyAuthenticated
+from core.category.models import Category
 
 from .models import Product
 from .serializers import ProductCreateUpdateSerializer, ProductSerializer
@@ -71,3 +75,72 @@ class ProductViewSet(viewsets.ModelViewSet):
         instance.is_deleted = True
         instance.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BulkUploadAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            # Read JSON file from request
+            file = request.FILES.get("file")
+            if not file:
+                return Response(
+                    {"error": "No file uploaded"}, status=status.HTTP_400_BAD_REQUEST
+                )
+
+            data = json.load(file)
+            print("\n------------data: ", data)
+
+            # Process Categories
+            for category_data in data.get("categories", []):
+                category, created = Category.objects.update_or_create(
+                    category_name=category_data.get("category_name"),
+                    defaults={
+                        "description": category_data.get("description", ""),
+                        "created_at": category_data.get("created_at", None),
+                        "updated_at": category_data.get("updated_at", None),
+                    },
+                )
+
+            # Process Products
+            product_instances = []
+            for product_data in data.get("products", []):
+                category = Category.objects.filter(
+                    category_name=product_data.get("category_name")
+                ).first()
+                if not category:
+                    return Response(
+                        {
+                            "error": f"Category with name {product_data.get("category_name")} not found"
+                        },
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+
+                product_instances.append(
+                    Product(
+                        category=category,
+                        product_name=product_data["product_name"],
+                        product_description=product_data.get("product_description", ""),
+                        product_price=product_data["product_price"],
+                        currency=product_data["currency"],
+                        stock_quantity=product_data["stock_quantity"],
+                        sku=product_data["sku"],
+                        image=product_data["image_url"],
+                        created_at=product_data.get("created_at", None),
+                        updated_at=product_data.get("updated_at", None),
+                    )
+                )
+
+            Product.objects.bulk_create(product_instances, ignore_conflicts=True)
+
+            return Response(
+                {"message": "Bulk upload successful"}, status=status.HTTP_201_CREATED
+            )
+
+        except json.JSONDecodeError:
+            return Response(
+                {"error": "Invalid JSON format"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        except Exception as e:
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
